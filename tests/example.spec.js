30f0ae723e37c5d1aae26baf36e751e9b535c6bc
Shabaab Hanaan <shababhanaan22@gmail.com>
2026-03-10 13:05:07 +0530

    Refactor homepage test and remove redundant test cases

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,12 +1,8 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('Playwright homepage tests', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  await page.getByRole('link', { name: 'Get started' }).click();
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+
