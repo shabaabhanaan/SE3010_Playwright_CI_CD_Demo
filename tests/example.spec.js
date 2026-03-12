5dc88deda156a1a1f42bc50eea693cf317959383
Shabaab Hanaan <shababhanaan22@gmail.com>
2026-03-12 11:03:21 +0530

    commit

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-//ci
+//ci cd
 test('Playwright homepage tests', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   await expect(page).toHaveTitle(/Playwright/);
